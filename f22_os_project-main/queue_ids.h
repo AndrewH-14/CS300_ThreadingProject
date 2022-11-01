234053e6bde6609ef64a0546096b3887a7d552d4
Andrew Hankins <67705231+AndrewH-14@users.noreply.github.com>
2022-11-01 08:43:06 -0500

    trying new file.

--- f22_os_project-main/queue_ids.h
+++ f22_os_project-main/queue_ids.h
@@ -1,3 +1,3 @@
 //#define FILE_IN_HOME_DIR "/Users/andrewhankins/Documents/Code/CS300_ThreadingProject/f22_os_project-main/achankins"
-#define FILE_IN_HOME_DIR "/home/achankins/CS300_ThreadingProject"
+#define FILE_IN_HOME_DIR "/home/achankins/pthread.c"
 #define QUEUE_NUMBER 0x14
